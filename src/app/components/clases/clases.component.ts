440f179c39555dd2eb16a807513d687842a4e398
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-07-02 18:57:44 -0400

    generando nuevo componente clases y colocando estilos

---
+++ src/app/components/clases/clases.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-clases',
+  templateUrl: './clases.component.html',
+})
+export class ClasesComponent implements OnInit {
+  alerta: string = 'alert-primary';
+  constructor() {}
+
+  ngOnInit(): void {}
+}
