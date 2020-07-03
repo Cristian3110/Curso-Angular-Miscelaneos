71e6010195d111cd760bada4f97d0773487a6370
Makoto3110 <cristiancarrillo311087@gmail.com>
2020-07-03 01:25:43 -0400

    configurando las rutas hijas en un archivo en específico (optimización)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,21 +2,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
 
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+// import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+// import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+// import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+
+// lo importamos de las rutas
+
+import { USUARIOS_ROUTES } from './components/usuario/usuario.routes';
 
 const ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   {
     path: 'usuario/:id',
     component: UsuarioComponent,
-    children: [
-      { path: 'nuevo', component: UsuarioNuevoComponent },
-      { path: 'editar', component: UsuarioEditarComponent },
-      { path: 'detalle', component: UsuarioDetalleComponent },
-      { path: '**', pathMatch: 'full', redirectTo: 'nuevo' },
-    ],
+    children: USUARIOS_ROUTES,
+    // { path: 'nuevo', component: UsuarioNuevoComponent },
+    // { path: 'editar', component: UsuarioEditarComponent },
+    // { path: 'detalle', component: UsuarioDetalleComponent },
+    // { path: '**', pathMatch: 'full', redirectTo: 'nuevo' },
   },
   { path: '**', pathMatch: 'full', redirectTo: 'home' },
 ];
